8e01da3ec8d25f740b1b7a43fc4ad4a7124afa73
luiz.borges <luizhenrique.borges@saude.ba.gov.br>
2024-03-24 10:03:52 -0300

    Extra credit 2 - types and hits ENTER

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -81,13 +81,16 @@ describe("Hacker Stories", () => {
     const newTerm = "Cypress";
 
     beforeEach(() => {
+      cy.intercept("GET", `**/search?query=${newTerm}&page=0`).as(
+        "getNewTermStories"
+      );
       cy.get("#search").clear();
     });
 
-    it("types and hits ENTER", () => {
+    it.only("types and hits ENTER", () => {
       cy.get("#search").type(`${newTerm}{enter}`);
 
-      cy.assertLoadingIsShownAndHidden();
+      cy.wait("@getNewTermStories");
 
       cy.get(".item").should("have.length", 20);
       cy.get(".item").first().should("contain", newTerm);
