7f7d5f8e66cec2806978d15b9dac627bd5383242
luiz.borges <luizhenrique.borges@saude.ba.gov.br>
2024-04-02 21:07:49 -0300

    Extra credit 1 - types and hits ENTER + types and clicks the submit button

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -55,77 +55,85 @@ describe("Hacker Stories", () => {
   });
 
   context("Mocking the API", () => {
-    beforeEach(() => {
-      cy.intercept("GET", `**/search?query=${initialTerm}&page=0`, {
-        fixture: "stories",
-      }).as("getStories");
+    context("Footer and list of stories", () => {
+      beforeEach(() => {
+        cy.intercept("GET", `**/search?query=${initialTerm}&page=0`, {
+          fixture: "stories",
+        }).as("getStories");
 
-      cy.visit("/");
-      cy.wait("@getStories");
-    });
-
-    it("shows the footer", () => {
-      cy.get("footer")
-        .should("be.visible")
-        .and("contain", "Icons made by Freepik from www.flaticon.com");
-    });
-
-    context("List of stories", () => {
-      // Since the API is external,
-      // I can't control what it will provide to the frontend,
-      // and so, how can I assert on the data?
-      // This is why this test is being skipped.
-      // TODO: Find a way to test it out.
-      it.skip("shows the right data for all rendered stories", () => {});
-
-      it("shows only less story after dimissing the first one", () => {
-        cy.get(".button-small").first().click();
-
-        cy.get(".item").should("have.length", 1);
+        cy.visit("/");
+        cy.wait("@getStories");
       });
 
-      // Since the API is external,
-      // I can't control what it will provide to the frontend,
-      // and so, how can I test ordering?
-      // This is why these tests are being skipped.
-      // TODO: Find a way to test them out.
-      context.skip("Order by", () => {
-        it("orders by title", () => {});
+      it("shows the footer", () => {
+        cy.get("footer")
+          .should("be.visible")
+          .and("contain", "Icons made by Freepik from www.flaticon.com");
+      });
 
-        it("orders by author", () => {});
+      context("List of stories", () => {
+        // Since the API is external,
+        // I can't control what it will provide to the frontend,
+        // and so, how can I assert on the data?
+        // This is why this test is being skipped.
+        // TODO: Find a way to test it out.
+        it.skip("shows the right data for all rendered stories", () => {});
 
-        it("orders by comments", () => {});
+        it("shows only less story after dimissing the first one", () => {
+          cy.get(".button-small").first().click();
 
-        it("orders by points", () => {});
+          cy.get(".item").should("have.length", 1);
+        });
+
+        // Since the API is external,
+        // I can't control what it will provide to the frontend,
+        // and so, how can I test ordering?
+        // This is why these tests are being skipped.
+        // TODO: Find a way to test them out.
+        context.skip("Order by", () => {
+          it("orders by title", () => {});
+
+          it("orders by author", () => {});
+
+          it("orders by comments", () => {});
+
+          it("orders by points", () => {});
+        });
       });
     });
 
     context("Search", () => {
       beforeEach(() => {
-        cy.intercept("GET", `**/search?query=${newTerm}&page=0`).as(
-          "getNewTermStories"
-        );
+        cy.intercept("GET", `**/search?query=${initialTerm}&page=0`, {
+          fixture: "empty",
+        }).as("getEmptyStories");
+
+        cy.intercept("GET", `**/search?query=${newTerm}&page=0`, {
+          fixture: "stories",
+        }).as("getStories");
+
+        cy.visit("/");
+        cy.wait("@getEmptyStories");
+
         cy.get("#search").clear();
       });
 
       it("types and hits ENTER", () => {
         cy.get("#search").type(`${newTerm}{enter}`);
 
-        cy.wait("@getNewTermStories");
+        cy.wait("@getStories");
 
-        cy.get(".item").should("have.length", 20);
-        cy.get(".item").first().should("contain", newTerm);
+        cy.get(".item").should("have.length", 2);
         cy.get(`button:contains(${initialTerm})`).should("be.visible");
       });
 
-      it("types and clicks the submit button", () => {
+      it.only("types and clicks the submit button", () => {
         cy.get("#search").type(newTerm);
         cy.contains("Submit").click();
 
-        cy.wait("@getNewTermStories");
+        cy.wait("@getStories");
 
-        cy.get(".item").should("have.length", 20);
-        cy.get(".item").first().should("contain", newTerm);
+        cy.get(".item").should("have.length", 2);
         cy.get(`button:contains(${initialTerm})`).should("be.visible");
       });
 
